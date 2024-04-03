50ae448c10f6671a909f1071df3c6de8a2477587
NAMRATHA SHETTY <NAMRATHA SHETTY@DESKTOP-F8OCNCO>
2024-04-03 17:25:44 +0530

    Added 2 files

--- Demo1/src/test/java/Scripts_Execution/Test_005_Meghana_Test.java
+++
@@ -1,7 +0,0 @@
-package Scripts_Execution;
-
-public class Test_005_Meghana_Test {
-public static void main(String[] args) {
-	
-}
-}
---
+++ Demo1/src/test/java/Scripts_Execution/Test_005_Meghanz_Test.java
@@ -0,0 +1,10 @@
+package Scripts_Execution;
+
+public class Test_005_Meghanz_Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
